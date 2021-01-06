9d2655327282eb7ae876349ba0d6a2c2b074d380
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2021-01-06 17:23:41 -0300

    Update display.py

--- display.py
+++ display.py
@@ -1,15 +1,18 @@
 import PySimpleGUI as sg
 from pytube import YouTube
+import time
+
 
 class TelaPython:
     sg.theme('Default')
 
     def __init__(self):
         layout = [
-            [sg.Text('Link',size=(4,0)),sg.Input(size=(34,0),key='link')],
-            [sg.Checkbox('Video',key='video'),sg.Checkbox('Audio',key='audio')],
+            [sg.Text('Link', size=(4, 0)), sg.Input(size=(34, 0), key='link')],
+            [sg.Checkbox('Video', key='video'),
+             sg.Checkbox('Audio', key='audio')],
             [sg.Button('Procurar video')],
-            [sg.Output(size=(40,15))]
+            [sg.Output(size=(40, 15))]
         ]
         self.janela = sg.Window("YouTube Downloader").layout(layout)
 
@@ -22,19 +25,23 @@ class TelaPython:
             audio = self.values['audio']
 
             print('Procurando seu vídeo...')
-            yt = YouTube(link)
 
-            print('Analizando...')
-            print(f'Titulo: {yt.title}')
-            print(f'Tempo: {yt.length} secundos')
+            try:
+                yt = YouTube(link)
+                print('Analizando...')
+                print(f'Titulo: {yt.title}')
+                print(f'Tempo: {yt.length} secundos')
 
-            print(30 * '=')
+                print(30 * '=')
 
-            ys = yt.streams.get_highest_resolution()
+                ys = yt.streams.get_highest_resolution()
 
-            print('Downloading...')
-            ys.download('Videos')
-            print('Download completo!!')
+                print('Downloading...')
+                ys.download('Videos')
+                print('Download completo!!')
+            except:
+                print('[ERRO] Video não encontrado no YouTube!')
+            # yt = YouTube(link)
 
 
 tela = TelaPython()
