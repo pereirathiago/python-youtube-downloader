553ffe076bf5583fbcd36ef773599ad71383b701
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2021-01-06 10:28:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,31 @@ from pytube import YouTube
 link = input('Cole aqui o link: ')
 yt = YouTube(link)
 
+'''
 # Titulo do video
 print(f'Título: {yt.title}')
+# numero de views do video
+print(f'Numero de views: {yt.views}')
+# Tempo do video
+print(f'Tempo: {yt.length} secundos')
+# Descrição do video
+print(f'Descrição do video: {yt.description}')
+# Avaliação
+print(f'Avaliação: {yt.rating}')
+
+# printing todos os streams possível'''
+# print(yt.streams)
+
+# somente audio
+# print(yt.streams.filter(only_audio=True))
+
+# somente video
+# print(yt.streams.filter(only_video=True))
+
+# print(yt.streams.filter(progressive=True))
+
+ys = yt.streams.get_highest_resolution()
+# ys = yt.streams.get_by_itag('22')
+print('Downloading...')
+ys.download('Videos')
+print('Download completed!!')
