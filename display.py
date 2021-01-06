bf6c0f583fbea435c0ad8bbaae42235accb8395a
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2021-01-06 19:48:47 -0300

    Update display.py

--- display.py
+++ display.py
@@ -1,5 +1,8 @@
 import PySimpleGUI as sg
 from pytube import YouTube
+import youtube_dl
+import os
+import time
 
 
 class TelaPython:
@@ -25,22 +28,56 @@ class TelaPython:
 
             print('Procurando seu vídeo...')
 
-            try:
-                yt = YouTube(link)
-                print('Analizando...')
-                print(f'Titulo: {yt.title}')
-                print(f'Tempo: {yt.length} secundos')
-
+            if audio == False and video == False:
+                print('[ERRO] Selecione uma opção(Audio/Video)')
                 print(30 * '=')
+            else:
+                try:
+                    yt = YouTube(link)
+                    print('Analizando...')
+                    time.sleep(3)
+                    print(f'Titulo: {yt.title}')
+                    print(f'Tempo: {yt.length} secundos')
 
-                ys = yt.streams.get_highest_resolution()
+                    print(30 * '=')
 
-                print('Downloading...')
-                ys.download('Videos')
-                print('Download completo!!')
-            except:
-                print('[ERRO] Video não encontrado no YouTube!')
-            # yt = YouTube(link)
+                    if video == True:
+                        ys = yt.streams.get_highest_resolution()
+                        print('Downloading...')
+                        ys.download('Videos')
+                    elif audio == True:
+                        ys = yt.streams.get_by_itag('140')
+                        print('Downloading...')
+                        video_info = youtube_dl.YoutubeDL().extract_info(
+                            url=link, download=False
+                        )
+
+                        path_to_save = 'Audios'
+
+                        where_to_save = os.getcwd()
+                        if path_to_save != "":
+                            where_to_save = f"{path_to_save}"
+
+                        filename = f"{video_info['title']}.mp3"
+                        output_path = os.path.join(where_to_save, filename)
+
+                        options = {
+                            'format': 'bestaudio/best',
+                            'keepvideo': False,
+                            'outtmpl': output_path,
+                            'postprocessors': [{
+                                'key': 'FFmpegExtractAudio',
+                                'preferredcodec': 'mp3',
+                                'preferredquality': '192',
+                            }]
+                        }
+                        with youtube_dl.YoutubeDL(options) as ydl:
+                            ydl.download([video_info['webpage_url']])
+
+                    print('Download completo!!')
+                except:
+                    print(
+                        '[ERRO] Video não encontrado no YouTube ou erro ao baixar!')
 
 
 tela = TelaPython()
